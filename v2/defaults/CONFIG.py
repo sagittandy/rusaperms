6e1c9119243eb621843da9b839c44ab8a5af500d
Michal Young <michal.young@gmail.com>
2016-12-19 12:59:23 -0800

    Cleanup: 'defaults' holds default CONFIG.py, while the 'real' one is not under revision control

---
+++ v2/defaults/CONFIG.py
@@ -0,0 +1,7 @@
+"""
+Configuration values for RUSA permanent map generation
+"""
+latlon_auto_csv_file="data/latlon_auto.csv"
+#latlon_auto_csv_file="test_data/latlon_auto.csv"
+latlon_manual_csv_file="data/latlon_manual.csv"
+#latlon_manual_csv_file="test_data/latlon_manual.csv"
